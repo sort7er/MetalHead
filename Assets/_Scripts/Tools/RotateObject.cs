10948d175664202c1b1b7576be4ff493bc30102e
sort7er <98805454+sort7er@users.noreply.github.com>
2023-02-17 11:33:00 +0100

    Bolt and nut pickup in the game

--- Assets/_Scripts/Tools/RotateObject.cs
+++ Assets/_Scripts/Tools/RotateObject.cs
@@ -4,6 +4,18 @@ public class RotateObject : MonoBehaviour
 {
     public float rotationSpeedX, rotationSpeedY, rotationSpeedZ;
 
+    public bool randomizeRotation;
+
+    private void Start()
+    {
+        if (randomizeRotation)
+        {
+            rotationSpeedX = Random.Range(-50, 50);
+            rotationSpeedY = Random.Range(-50, 50);
+            rotationSpeedZ = Random.Range(-50, 50);
+        }
+    }
+
     private void Update()
     {
         transform.Rotate(new Vector3(rotationSpeedX, rotationSpeedY, rotationSpeedZ) * Time.deltaTime);
