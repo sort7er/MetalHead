8ced29570c69464fe3725df59b7183526df39389
sort7er <98805454+sort7er@users.noreply.github.com>
2023-02-17 16:00:40 +0100

    PickupCanBeStopped

--- Assets/_Scripts/Pickups/Pickup.cs
+++ Assets/_Scripts/Pickups/Pickup.cs
@@ -3,60 +3,72 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public float lifeTime, startFlashTime;
-    public MeshRenderer pickup;
+    public MeshRenderer pickupMesh;
     public ParticleSystem effect;
 
+    private Animator pickUpAnim;
     private float timer;
     private float flashTime;
-    private bool flashStarted;
+    private bool flashStarted, pickUp;
 
     private void Start()
     {
+        pickUpAnim = GetComponent<Animator>();
         timer = lifeTime;
     }
 
     private void Update()
     {
-        if(timer > 0)
+        if (!pickUp)
         {
-            timer -= Time.deltaTime;
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
 
-            if (timer < 2)
-            {
-                flashTime = startFlashTime / 4;
-            }
-            else if (timer < 4)
-            {
-                flashTime = startFlashTime / 2;
-            }
-            else if(timer < 6)
-            {
-                if (!flashStarted)
+                if (timer < 2)
                 {
-                    FlashOff();
-                    flashStarted = true;
+                    flashTime = startFlashTime / 4;
                 }
-                flashTime = startFlashTime;
+                else if (timer < 4)
+                {
+                    flashTime = startFlashTime / 2;
+                }
+                else if (timer < 6)
+                {
+                    if (!flashStarted)
+                    {
+                        FlashOff();
+                        flashStarted = true;
+                    }
+                    flashTime = startFlashTime;
+                }
             }
-        }
-        else
-        {
-            effect.transform.parent = ParentManager.instance.effects;
-            effect.Stop();
-            Destroy(effect, 2);
-            Destroy(gameObject);
+            else
+            {
+                effect.transform.parent = ParentManager.instance.effects;
+                effect.Stop();
+                Destroy(effect, 2);
+                Destroy(gameObject);
+            }
         }
     }
 
     private void FlashOff()
     {
-        pickup.enabled = false;
+        pickupMesh.enabled = false;
         Invoke("FlashOn", flashTime/2);
     }
     private void FlashOn()
     {
-        pickup.enabled = true;
+        pickupMesh.enabled = true;
         Invoke("FlashOff", flashTime);
+    }
+    public void PickUp()
+    {
+        pickUp = true;
+        CancelInvoke();
+        pickUpAnim.enabled = false;
+        pickupMesh.enabled = true;
     }
 
 }
