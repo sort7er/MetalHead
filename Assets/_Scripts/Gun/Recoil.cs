c022f3fe2cc523d5d333d62aa5c8f381b1d0a926
sort7er <98805454+sort7er@users.noreply.github.com>
2023-02-21 21:01:32 +0100

    Fixed recoil a bit

--- Assets/_Scripts/Gun/Recoil.cs
+++ Assets/_Scripts/Gun/Recoil.cs
@@ -1,12 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class Recoil : MonoBehaviour
 {
-    public float smoothTimeUp, smoothTimeDown, lowestAngle, highestAngle;
+    public float smoothTimeUp, smoothTimeDown, lowestAngle, highestAngle, recoilHeight;
     public Transform leftHandAttach, rightHandAttach;
 
     private bool left, recoil, moveUp;
-    private float currentRotation, targetRotation, velocity;
+    private float currentRotation, currentPosition, targetRotation, targetPosition, velocity, velocity2;
 
 
     private void Update()
@@ -14,19 +15,23 @@ public class Recoil : MonoBehaviour
         if (left && recoil)
         {
             leftHandAttach.localRotation = Quaternion.Euler(currentRotation, 0, 0);
-            
+            leftHandAttach.localPosition = new Vector3(0, currentPosition, 0);
+
             if (moveUp)
             {
                 currentRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref velocity, Time.deltaTime * smoothTimeUp);
+                currentPosition = Mathf.SmoothDamp(currentPosition, targetPosition, ref velocity2, Time.deltaTime * smoothTimeUp);
                 if (currentRotation <= targetRotation + 0.1f)
                 {
                     moveUp = false;
+                    targetPosition = 0;
                     targetRotation = 0;
                 }
             }
             else
             {
                 currentRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref velocity, Time.deltaTime * smoothTimeDown);
+                currentPosition = Mathf.SmoothDamp(currentPosition, targetPosition, ref velocity2, Time.deltaTime * smoothTimeDown);
                 if (leftHandAttach.localRotation.x >= 0)
                 {
                     leftHandAttach.localRotation = Quaternion.Euler(0, 0, 0);
@@ -37,19 +42,23 @@ public class Recoil : MonoBehaviour
         else if (!left && recoil)
         {
             rightHandAttach.localRotation = Quaternion.Euler(currentRotation, 0, 0);
+            rightHandAttach.localPosition = new Vector3(0, currentPosition, 0);
 
             if (moveUp)
             {
                 currentRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref velocity, Time.deltaTime * smoothTimeUp);
+                currentPosition = Mathf.SmoothDamp(currentPosition, targetPosition, ref velocity2, Time.deltaTime * smoothTimeUp);
                 if (currentRotation <= targetRotation + 0.1f)
                 {
                     moveUp = false;
+                    targetPosition = 0;
                     targetRotation = 0;
                 }
             }
             else
             {
                 currentRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref velocity, Time.deltaTime * smoothTimeDown);
+                currentPosition = Mathf.SmoothDamp(currentPosition, targetPosition, ref velocity2, Time.deltaTime * smoothTimeDown);
                 if (rightHandAttach.localRotation.x >= 0)
                 {
                     rightHandAttach.localRotation = Quaternion.Euler(0, 0, 0);
@@ -73,6 +82,7 @@ public class Recoil : MonoBehaviour
             left = false;
         }
         targetRotation = -Random.Range(lowestAngle, highestAngle);
+        targetPosition = recoilHeight;
         moveUp = true;
         recoil = true;
     }
