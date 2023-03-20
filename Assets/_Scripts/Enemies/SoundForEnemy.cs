bcdb7655c0ff5fec96dfc8a691b4a3bd5454dcd3
sort7er <98805454+sort7er@users.noreply.github.com>
2023-03-20 02:10:08 +0100

    SOund is more randomized

--- Assets/_Scripts/Enemies/SoundForEnemy.cs
+++ Assets/_Scripts/Enemies/SoundForEnemy.cs
@@ -7,18 +7,34 @@ public class SoundForEnemy : MonoBehaviour
     public AudioSource enemySource;
     public AudioSource enemySource2;
 
+    private int justPlayed;
+
     private bool turn;
 
     public void Step()
     {
         if (turn)
         {
-            enemySource.PlayOneShot(steps[Random.Range(0, steps.Length)]);
+            int randomNumber = Random.Range(0, steps.Length);
+            while(justPlayed == randomNumber)
+            {
+                randomNumber = Random.Range(0, steps.Length);
+            }
+            
+            enemySource.PlayOneShot(steps[randomNumber]);
+            justPlayed = randomNumber;
             turn = false;
         }
         else
         {
-            enemySource2.PlayOneShot(steps[Random.Range(0, steps.Length)]);
+            int randomNumber = Random.Range(0, steps.Length);
+            while (justPlayed == randomNumber)
+            {
+                randomNumber = Random.Range(0, steps.Length);
+            }
+
+            enemySource2.PlayOneShot(steps[randomNumber]);
+            justPlayed = randomNumber;
             turn = true;
         }
     }
