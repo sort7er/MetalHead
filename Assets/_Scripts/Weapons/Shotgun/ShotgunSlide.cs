04e12df8332cf0c0b11d654b491a05ae00cef400
sort7er <98805454+sort7er@users.noreply.github.com>
2023-05-14 08:38:37 +0200

    Started on slide back

--- Assets/_Scripts/Weapons/Shotgun/ShotgunSlide.cs
+++ Assets/_Scripts/Weapons/Shotgun/ShotgunSlide.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -5,15 +6,92 @@ public class ShotgunSlide : MonoBehaviour
 {
     public SoundForGun soundForGun;
     public Transform attachPoint;
+    public float threshold, sensitivity, limit;
 
     private XRSimpleInteractable simpleInteractable;
+    private Tac14 tac14;
     private Vector3 startPos;
-    private bool slideValid;
-
+    private Transform leftHand, rightHand;
+    private Vector3 distanceAtThreshold;
+    private bool slideValid, follow, left;
+    public float initialDistance, difference;
     private void Start()
     {
-        startPos = transform.position;
+        tac14 = GetComponentInParent<Tac14>();
+        startPos = transform.localPosition;
         simpleInteractable = GetComponent<XRSimpleInteractable>();
+    }
+
+    private void Update()
+    {
+        if (!tac14.auto && simpleInteractable.enabled)
+        {
+            rightHand = GameManager.instance.rHand.transform;
+            leftHand = GameManager.instance.lHand.transform;
+
+
+            if (left)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + GameManager.instance.leftHand.transform.localPosition.z - transform.localPosition.z);
+            }
+            //else
+            //{
+            //    transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - difference * sensitivity);
+            //}
+
+
+            //if (Vector3.Distance(leftHand.position, rightHand.position) < initialDistance - threshold)
+            //{
+            //    if (!follow)
+            //    {
+            //        follow = true;
+            //        if (left)
+            //        {
+            //            distanceAtThreshold = GameManager.instance.lHand.transform.position;
+            //        }
+            //        else
+            //        {
+            //            distanceAtThreshold = GameManager.instance.rHand.transform.position;
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    if (follow)
+            //    {
+            //        follow = false;
+            //        transform.localPosition = startPos;
+            //    }
+            //}
+        }
+        
+        //if (follow)
+        //{
+            
+
+        //    if (left)
+        //    {
+        //        difference = (distanceAtThreshold - GameManager.instance.lHand.transform.position).magnitude;
+        //        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - difference * sensitivity);
+        //    }
+        //    else
+        //    {
+        //        difference = (distanceAtThreshold - GameManager.instance.rHand.transform.position).magnitude;
+        //        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - difference * sensitivity);
+        //    }
+
+        //    //if (transform.localPosition.z < limit)
+        //    //{
+        //    //    if (follow)
+        //    //    {
+        //    //        soundForGun.Slide();
+        //    //    }
+        //    //    follow = false;
+        //    //    slideValid = true;
+        //    //}
+
+        //}
+
     }
 
     public void SlideDone()
@@ -28,14 +106,18 @@ public class ShotgunSlide : MonoBehaviour
     {
         if (GameManager.instance.CheckGameObject(gameObject) == 1)
         {
+            left = true;
             GameManager.instance.leftHand.GrabShotgunSlide(true);
             GameManager.instance.leftHand.NewParent(attachPoint, attachPoint);
         }
         if (GameManager.instance.CheckGameObject(gameObject) == 2)
         {
+            left = false;
             GameManager.instance.rightHand.GrabShotgunSlide(true);
             GameManager.instance.rightHand.NewParent(attachPoint, attachPoint);
         }
+
+        initialDistance = Vector3.Distance(rightHand.position, leftHand.position);
     }
     public void ReleaseSlide()
     {
